92f256aefdb5d9183434153d62340c6f169d50b2
tomeklarysz <ttomek.larysz@gmail.com>
2025-04-01 18:38:12 +0200

    messages and phoneHelpBlock added

--- forms.py
+++ forms.py
@@ -3,8 +3,17 @@ from wtforms import StringField, IntegerField, TelField, SubmitField
 from wtforms.validators import DataRequired, Email, NumberRange, Regexp
 
 class registerForm(FlaskForm):
-    name = StringField("Imię", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    age = IntegerField("Wiek", validators=[DataRequired(), NumberRange(min=18, max=100)])
-    phone = TelField("Numer telefonu", validators=[DataRequired(), Regexp("[0-9]{3}-[0-9]{3}-[0-9]{3}")])
+    name = StringField("Imię", validators=[DataRequired(message="To pole jest wymagane.")])
+    email = StringField("Email", validators=[DataRequired(message="Podaj poprawny adres email."), Email()])
+    age = IntegerField("Wiek", validators=[DataRequired(message="Podaj wiek między 18 a 100 lat."), NumberRange(min=18, max=100, message="Podaj wiek między 18 a 100 lat.")])
+    phone = TelField(
+        "Numer telefonu", 
+        validators=[
+            DataRequired(message="Podaj poprawny numer telefonu."), 
+            Regexp("[0-9]{3}-[0-9]{3}-[0-9]{3}", message="Podaj poprawny numer telefonu.")
+        ],
+        render_kw={
+            "aria-describedby": "phoneHelpBlock"
+        }
+    )
     submit = SubmitField("Wyślij")
